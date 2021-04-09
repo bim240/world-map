51029af4d665d6a25092f3e455c760cdfbe04d13
bim240 <bimlendu357@gmail.com>
2021-04-09 21:01:22 +0530

    initial setup complete

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,33 +1,15 @@
-import axios from 'axios';
-import { SERVER_DOMAIN } from './constants';
-import {
-  authRequestInterceptor,
-  authResponseInterceptor,
-} from 'middleware/auth';
-import history from 'utils/history';
+import axios from "axios";
 
 // const VERSION_CODE = 'v0';
 
 // this can only be used for auth!
 const authInstance = axios.create({
-  baseURL: `${SERVER_DOMAIN}`,
+  baseURL: `https://wikiapi.p.rapidapi.com/api/v1/wiki/geography/country/info`,
+  headers: {
+    "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    "x-rapidapi-host": "wikiapi.p.rapidapi.com",
+    useQueryString: true,
+  },
 });
-
-// authInstance.interceptors.request.use(encryptionIntercepter);
-
-authInstance.interceptors.request.use(authRequestInterceptor);
-
-authInstance.interceptors.response.use(
-  authResponseInterceptor,
-  function (error) {
-    const { response } = error;
-    if (response && response.status === 401) {
-      window.localStorage.removeItem('authtoken');
-      window.localStorage.removeItem('onboardingState');
-      history.push('/auth', { inactivity: true });
-    }
-    return response;
-  }
-);
 
 export { authInstance as axiosAuth };
